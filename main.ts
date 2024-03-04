6bd37f93227a4dd4be02e35036c0d217d2e7b38e
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-04 21:23:47 +0000

    progress.

--- main.ts
+++ main.ts
@@ -13,12 +13,58 @@ export default class AutoLinkPlugin extends Plugin {
 		this.addSettingTab(new AutoLinkPluginSettingsTab(this.app, this));
 
 		this.registerDomEvent(document, "keydown", (event: KeyboardEvent) => {
-			if (/\s/.test(event.key)) {
-				const markdownView =
+			if (!/\s/.test(event.key)) {
+				return;
+			}
+
+			const openFile = this.app.workspace.getActiveFile();
+
+			if (!openFile) {
+				return;
+			}
+
+			this.app.vault.read(openFile).then((content) => {
+				console.log("content = ", content);
+
+				const activeView =
 					this.app.workspace.getActiveViewOfType(MarkdownView);
 
-				console.log(markdownView?.getDisplayText());
-			}
+				if (!activeView || activeView.getMode() !== "source") {
+					return;
+				}
+
+				const editor = activeView.editor;
+
+				const cursor = editor.getCursor();
+
+				console.log("cursor = ", cursor);
+
+				const contentLines = content.split("\n");
+
+				console.log("contentLines = ", contentLines);
+
+				const currentLine = contentLines[cursor.line];
+
+				console.log("currentLine = ", currentLine);
+
+				let currentCharacterIndex = cursor.ch;
+				let previousWord = "";
+
+				while (currentCharacterIndex >= 2) {
+					const currentCharacter = currentLine[currentCharacterIndex];
+
+					if (/\s/.test(currentCharacter)) {
+						break;
+					}
+
+					previousWord = currentCharacter + previousWord;
+					currentCharacterIndex--;
+				}
+
+				console.log("previousWord = ", previousWord);
+
+				this.app.vault.adapter.write(openFile.path, "new content");
+			});
 		});
 	}
 
